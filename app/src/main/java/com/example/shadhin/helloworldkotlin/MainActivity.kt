95164e20189b93f356b1c156b319f7344e51d863
ShadhiN <shadhinemail@gmail.com>
2019-01-07 14:31:50 +0600

    Basic data pass to another activity using kotlin

--- app/src/main/java/com/example/shadhin/helloworldkotlin/MainActivity.kt
+++ app/src/main/java/com/example/shadhin/helloworldkotlin/MainActivity.kt
@@ -1,12 +1,58 @@
 package com.example.shadhin.helloworldkotlin
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
+    //init variable
+    private var etxtUserName: EditText? = null
+    private var etxtNickName: EditText? = null
+    private var etxPhoneNumer: EditText? = null
+    private var etxBirthday: EditText? = null
+    private var etxEmail: EditText? = null
+    private var etxPassword: EditText? = null
+    private var spPresentCountry: Spinner? = null
+    private var chkAgree: CheckBox? = null
+    private var raGender: RadioGroup? = null
+    private var btnLogin: Button? = null
+    private var btnReg: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+        define()
+    }
+    private fun define() {
+
+        btnReg!!.setOnClickListener {
+            val intent = Intent(this@MainActivity, RegSuccess::class.java)
+            intent.putExtra("etxtUserName", etxtUserName!!.text.toString())
+            intent.putExtra("etxtNickName", etxtNickName!!.text.toString())
+            intent.putExtra("etxPhoneNumer", etxPhoneNumer!!.text.toString())
+            intent.putExtra("etxBirthday", etxBirthday!!.text.toString())
+            intent.putExtra("etxEmail", etxEmail!!.text.toString())
+            intent.putExtra("etxPassword", etxPassword!!.text.toString())
+            //intent.putExtra("spPresentCountry", spPresentCountry!!.text.toString())
+            //intent.putExtra("chkAgree", chkAgree!!.text.toString())
+            //intent.putExtra("raGender", raGender!!.text.toString())
+            startActivity(intent)
+        }
+    }
+    private fun initView() {
+        etxtUserName = findViewById(R.id.etxtUserName) as EditText
+        etxtNickName = findViewById(R.id.etxtNickName) as EditText
+        etxPhoneNumer = findViewById(R.id.etxPhoneNumer) as EditText
+        etxBirthday = findViewById(R.id.etxBirthday) as EditText
+        etxEmail = findViewById(R.id.etxEmail) as EditText
+        etxPassword = findViewById(R.id.etxPassword) as EditText
+        spPresentCountry = findViewById(R.id.spPresentCountry) as Spinner
+        chkAgree = findViewById(R.id.chkAgree) as CheckBox
+        raGender = findViewById(R.id.raGender) as RadioGroup
+        btnLogin = findViewById(R.id.btnLogin) as Button
+        btnReg = findViewById(R.id.btnReg) as Button
+       // TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
---
+++ app/src/main/java/com/example/shadhin/helloworldkotlin/RegSuccess.kt
@@ -0,0 +1,74 @@
+package com.example.shadhin.helloworldkotlin
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView
+
+import kotlinx.android.synthetic.main.activity_reg_success.*
+
+class RegSuccess : AppCompatActivity() {
+    private var txtAfterLoginUserNameDisplay: TextView? = null
+    private var txtAfterLoginNameDisplay: TextView? = null
+    private var txtAfterLoginPhoneDisplay: TextView? = null
+    private var txtAfterLoginBirthdayDisplay: TextView? = null
+    private var txtAfterLoginEmail: TextView? = null
+    private var txtAfterLoginPassword: TextView? = null
+    private var txtAfterLoginGender: TextView? = null
+    private var txtAfterLoginCountry: TextView? = null
+    private var txtAgree: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reg_success)
+
+        initView()
+        receivingValue()
+
+        setSupportActionBar(toolbar)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    private fun receivingValue() {
+        val intent = intent
+        val userName = intent.getStringExtra("etxtUserName")
+        val nickName = intent.getStringExtra("etxtNickName")
+        val phoneNumer = intent.getStringExtra("etxPhoneNumer")
+        val birthday = intent.getStringExtra("etxBirthday")
+        val email = intent.getStringExtra("etxEmail")
+        val password = intent.getStringExtra("etxPassword")
+       // val presentCountry = intent.getStringExtra("spPresentCountry")
+       // val agree = intent.getStringExtra("chkAgree")
+        //val gender = intent.getStringExtra("raGender")
+
+        txtAfterLoginUserNameDisplay!!.text = userName
+        txtAfterLoginNameDisplay!!.text = nickName
+        txtAfterLoginPhoneDisplay!!.text = phoneNumer
+        txtAfterLoginBirthdayDisplay!!.text = birthday
+        txtAfterLoginEmail!!.text = email
+        txtAfterLoginPassword!!.text = password
+       // txtAfterLoginGender!!.text = presentCountry
+       // txtAfterLoginCountry!!.text = agree
+        //txtAgree!!.text = gender
+
+       // TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun initView() {
+        txtAfterLoginUserNameDisplay = findViewById(R.id.txtAfterLoginUserNameDisplay) as TextView
+        txtAfterLoginNameDisplay = findViewById(R.id.txtAfterLoginNameDisplay) as TextView
+        txtAfterLoginPhoneDisplay = findViewById(R.id.txtAfterLoginPhoneDisplay) as TextView
+        txtAfterLoginBirthdayDisplay = findViewById(R.id.txtAfterLoginBirthdayDisplay) as TextView
+        txtAfterLoginEmail = findViewById(R.id.txtAfterLoginEmail) as TextView
+        txtAfterLoginPassword = findViewById(R.id.txtAfterLoginPassword) as TextView
+        txtAfterLoginGender = findViewById(R.id.txtAfterLoginGender) as TextView
+        txtAfterLoginCountry = findViewById(R.id.txtAfterLoginCountry) as TextView
+        txtAgree = findViewById(R.id.txtAgree) as TextView
+
+       // TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
